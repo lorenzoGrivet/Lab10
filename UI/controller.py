59a15ca33a8cd158bb1c20d87a3521bc25426564
lorenzoGrivet <lori.grivet@gmail.com>
2024-05-14 17:44:05 +0200

    da finire

--- UI/controller.py
+++ UI/controller.py
@@ -1,3 +1,5 @@
+from operator import attrgetter
+
 import flet as ft
 
 
@@ -9,4 +11,32 @@ class Controller:
         self._model = model
 
     def handleCalcola(self, e):
-        pass
+        self._view._txt_result.controls.clear()
+        anno=int(self._view._txtAnno.value)
+        self._model.calcolaConfini(anno)
+
+        self._view._txt_result.controls.append(ft.Text(f"Grafo creato"))
+        self._view._txt_result.controls.append(ft.Text(f"Il grafo contiene {self._model.getNumNodes()} nodi"))
+        self._view._txt_result.controls.append(ft.Text(f"Il grafo contiene {self._model.getNumEdges()} archi"))
+
+        self._view._txt_result.controls.append(ft.Text(f"Componenti connesse: {(self._model.connesse)}"))
+
+
+        # gradi= dict(self._model.grafo.degree())
+        # ordinata=sorted(gradi,key=gradi.get,reverse=True)
+
+        # for nodo in self._model.grafo.nodes:
+        #     self._view._txt_result.controls.append(ft.Text(f"{self._model.idMap[nodo]}: {self._model.grafo.degree(nodo)} vicini"))
+
+        diz_ordinato=(sorted(self._model.idMap.items())
+
+        for a in diz_ordinato:
+            # print(self._model.idMap[a])
+
+            if self._model.e.__contains__(self._model.idMap[a].CCode):
+
+                self._view._txt_result.controls.append(ft.Text(f"{self._model.idMap[a]} -> {self._model.grafo.degree(a)}"))
+
+
+        self._view.update_page()
+
